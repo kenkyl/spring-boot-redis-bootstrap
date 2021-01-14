cbe5a9cb3ba0469d4c955a68bd4a96c9e191e355
kenkyl <kylekennedy@Kyles-MBP-2.attlocal.net>
2021-01-13 21:10:18 -0600

    bootstrapped with spring initializer

---
+++ src/main/java/com/redislabs/springredisbootstrap/SpringredisbootstrapApplication.java
@@ -0,0 +1,13 @@
+package com.redislabs.springredisbootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringredisbootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringredisbootstrapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redislabs/springredisbootstrap/SpringredisbootstrapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redislabs.springredisbootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringredisbootstrapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
